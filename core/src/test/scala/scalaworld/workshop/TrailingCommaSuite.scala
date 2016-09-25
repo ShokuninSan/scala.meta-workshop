5f937699416419676a6f1535eeb421d6f2c44c89
Shokunin San <me@flatmap.io>
2016-09-25 15:26:49 +0200

    Part 1 (Tokens): solved exercise #2.
    A naive implementation of stripping trailing commas on function parameters.

--- core/src/test/scala/scalaworld/workshop/TrailingCommaSuite.scala
+++ core/src/test/scala/scalaworld/workshop/TrailingCommaSuite.scala
@@ -10,9 +10,16 @@ import scalaworld.util.DiffAssertions
 class TrailingCommaSuite extends FunSuite with DiffAssertions {
 
   /** Removes all commas behind the last argument of function calls */
-  def stripTrailingCommas(tokens: Tokens): String = {
-    ""
-  }
+  def stripTrailingCommas(tokens: Tokens): String =
+    tokens.foldRight(List.empty[Token]){(token, tokens) =>
+      tokens match {
+        case (_: RightParen) :: _ |
+             (_: LF) :: (_: RightParen) :: _ |
+             (_: LF) :: (_: Comment) :: (_: LF) :: (_: RightParen) :: _
+          if token.isInstanceOf[Comma] => tokens
+        case _ => token :: tokens
+      }
+    }.mkString.tokenize.get.syntax
 
   def check(original: String, expected: String): Unit = {
     test(logger.reveal(original)) {
